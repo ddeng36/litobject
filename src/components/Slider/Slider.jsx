af754570d684eaaa5fbeba4c73fdc1dcfdaddace
Dingchao <dingchaodeng@gatech.edu>
2023-06-08 21:54:27 +0800

    Btn

--- src/components/Slider/Slider.jsx
+++ src/components/Slider/Slider.jsx
@@ -1,23 +1,19 @@
 import React, { useState, useEffect } from "react";
 import "./Slider.scss";
 import slidersImg from "../../constants/slidersImg";
-const Slider = () => {
-  const data = [68.2, 62.8, 57.6, 53.2, 51.7, 48.2, 44.2];
-  const data2 = [67.2, 60.9, 56.8, 53, 49.2, 46.1, 43.2];
-  const data3 = [66.2, 59.8, 55.8, 52.2, 48.2, 45.2, 42.2];
-  const data4 = [];
 
+const Slider = () => {
   const length = slidersImg.length;
   const [currentSlide, setCurrentSlide] = useState(0);
+
   useEffect(() => {
     const interval = setInterval(() => {
       setCurrentSlide((prev) => (prev === length - 1 ? 0 : prev + 1));
-    }, 3000);
+    }, 5000);
     return () => clearInterval(interval);
   }, [currentSlide]);
-  const style={
-    transform: `translateY(-${currentSlide * 100}%)`,
-  }
+
+
   return (
     <div className="slider">
       <div className="content">
@@ -25,14 +21,18 @@ const Slider = () => {
           <div
             key={index}
             className={index === currentSlide ? "slide active" : "slide"}
-            style={style}
           >
-            <img src={item} alt="slider" className="slide__img" />
+            <img
+              src={item}
+              alt="slider"
+              className="slide__img"
+            />
           </div>
         ))}
       </div>
     </div>
   );
 };
+
 
 export default Slider;
