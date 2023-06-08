74a373029c26d8ba84271f0e5908f5fec7187d1f
Dingchao <dingchaodeng@gatech.edu>
2023-06-08 15:27:42 +0800

    imgs

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.scss';
-
+import {images} from "./constants";
 function App() {
   return (
     <div className="App">
       test
+      <img src={images.xl1} alt="xl1" />
     </div>
   );
 }
