ed06415afb603dc4153135143fa4810f88cf5d15
Dingchao <dingchaodeng@gatech.edu>
2023-06-09 14:06:31 +0800

    lng btn

---
+++ src/components/language/Language.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import './Language.scss'
+const Language = () => {
+  return (
+    <div>Language</div>
+  )
+}
+
+export default Language
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,12 +2,17 @@ import React from "react";
 import "./Home.scss";
 import Navbar from "../../components/navbar/Navbar";
 import Slider from "../../components/slider/Slider";
+import Language from "../../components/language/Language";
+import NavButton from "../../components/navButton/NavButton";
 const Home = () => {
   return (
     <div className="home">
-      
       <Navbar />
       <Slider />
+      <div className="topRight">
+        <Language />
+        <NavButton />
+      </div>
     </div>
   );
 };
