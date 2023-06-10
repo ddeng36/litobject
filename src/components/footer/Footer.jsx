700eebb03f0d229c5e10b21004bc9f4064fd5fde
Dingchao <dingchaodeng@gatech.edu>
2023-06-10 17:19:39 +0800

    ft

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import './Footer.scss'
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className='footer'>Copyright © LitBuild All Rights Reserved.</div>
   )
 }
 
