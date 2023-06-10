685373d7410f675de95eab128d827ee8f8c9c0ac
Dingchao <dingchaodeng@gatech.edu>
2023-06-10 19:15:25 +0800

    filter

---
+++ src/constants/data.js
@@ -0,0 +1,53 @@
+export default {
+    "filterData": [
+        {
+            "id": 1,
+            "name": "All Projects"
+        },
+        {
+            "id": 2,
+            "name": "Electronics"
+        },
+        {
+            "id": 3,
+            "name": "Kitchenware"
+        },
+        {
+            "id": 4,
+            "name": "Furniture"
+        },
+        {
+            "id": 5,
+            "name": "Bathrooms"
+        },
+        {
+            "id": 6,
+            "name": "Lighting"
+        },
+        {
+            "id": 7,
+            "name": "Architectual"
+        },
+        {
+            "id": 8,
+            "name": "Car"
+        },
+        {
+            "id": 9,
+            "name": "Accessories"
+        },
+        {
+            "id": 10,
+            "name": "Exhibitions"
+        },
+        {
+            "id": 11,
+            "name": "Limited Editions"
+        },
+        {
+            "id": 12,
+            "name": "More"
+        }
+        
+    ]
+}
--- src/pages/projects/Projects.jsx
+++ src/pages/projects/Projects.jsx
@@ -1,14 +1,44 @@
-import React from 'react'
-import './Projects.scss'
-import Navbar from '../../components/navbar/Navbar'
-import Footer from '../../components/footer/Footer'
+import React,{useState} from "react";
+import "./Projects.scss";
+import Navbar from "../../components/navbar/Navbar";
+import Footer from "../../components/footer/Footer";
+import NavButton from "../../components/navButton/NavButton";
+import data from "../../constants/data";
 const Projects = () => {
+  const [categoty, setCategory] = useState(data.filterData[0].name);
+  console.log(data.filterData)
   return (
-    <div className='projects'>
+    <div className="projects">
+      <div className="top">
         <Navbar />
-        <Footer />
-    </div>
-  )
-}
+        <div className="button">
+          <NavButton />
+        </div>
+      </div>
+      <div className="center">
+        <div className="left">
+          <div className="filter">
+            <ul>
+              {data.filterData.map((item, index) => (
+                <li
 
-export default Projects
+                  key={index}
+                  onClick={() => setCategory(item.name)}
+                  className={categoty === item.name ? "active" : ""}
+                >
+                  <div>{item.name}</div>
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
+        <div className="right">1</div>
+      </div>
+      <div className="bottom">
+        <Footer />
+      </div>
+    </div>
+  );
+};
+
+export default Projects;
