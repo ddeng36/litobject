37bea58602be0f9e9d1ba682ffbfaef00a34f86c
Dingchao <dingchaodeng@gatech.edu>
2023-06-10 22:28:21 +0800

    card

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -1,15 +1,19 @@
-import React from "react";
+import React,{useState} from "react";
 import "./Card.scss";
 import { Link } from "react-router-dom";
 const Card = ({ item }) => {
+    const [hover,setHover]=useState(false)
   return (
-    <Link to={`/product/${item.id}`} className="link">
+    <Link to={`/product/${item.id}`} className="link"
+        onMouseEnter={()=>setHover(true)}
+        onMouseLeave={()=>setHover(false)}
+    >
       <div className="card">
         <div className="image">
-          <img src={item.img} alt="" className="mainImg" />
-          <img src={item.img2} alt="" className="secondImg" />
+          <img src={item.img} alt="" className={`mainImg ${hover ? "fade" : ""}`}  />
+          <img src={item.img2} alt="" className={`secondImg ${hover ? "active" : ""}`} />
         </div>
-        <span>{item.title}</span>
+        <div className="title">{item.title}</div>
       </div>
     </Link>
   );
