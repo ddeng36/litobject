786976b2ad05ea425c89cad234ea7a50980eb73f
Dingchao <dingchaodeng@gatech.edu>
2023-06-12 19:03:45 +0800

    xl5

--- src/constants/slidersImg.js
+++ src/constants/slidersImg.js
@@ -2,9 +2,11 @@ import xl1 from '../assets/images/xl1.jpeg';
 import xl2 from '../assets/images/xl2.jpeg';
 import xl3 from '../assets/images/xl3.jpeg';
 import xl4 from '../assets/images/xl4.jpeg';
+import xl5 from '../assets/images/xl5.jpeg';
 export default[
     xl1,
     xl2,
     xl3,
     xl4,
+    xl5
 ]
