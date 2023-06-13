798811b89ad1a8fb4d38e1973844421d434f857d
Dingchao <dingchaodeng@gatech.edu>
2023-06-13 16:30:16 +0800

    scene

--- src/pages/project/Project.jsx
+++ src/pages/project/Project.jsx
@@ -1,3 +1,4 @@
+import { render } from '@testing-library/react';
 import React, { useRef, useEffect } from 'react';
 import * as THREE from 'three';
 
@@ -6,6 +7,11 @@ const Project = () => {
   useEffect(() => {
     const renderer = new THREE.WebGLRenderer();
     renderer.setSize(window.innerWidth, window.innerHeight);
+    const scene = new THREE.Scene();
+    const camera = new THREE.PerspectiveCamera(
+      75, window.innerWidth / window.innerHeight, 0.1, 1000,
+    );
+    renderer.render(scene, camera);
     mount.current.appendChild(renderer.domElement);
   }, []);
 
