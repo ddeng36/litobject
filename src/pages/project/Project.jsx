da94c6967ea8fcc5ee1474cfc5dc0d124e2bcc7f
Dingchao <dingchaodeng@gatech.edu>
2023-06-13 17:03:31 +0800

    animate

--- src/pages/project/Project.jsx
+++ src/pages/project/Project.jsx
@@ -1,19 +1,41 @@
-import { render } from '@testing-library/react';
-import React, { useRef, useEffect } from 'react';
+import React, { useRef, useEffect, useState } from 'react';
 import * as THREE from 'three';
 
 const Project = () => {
   const mount = useRef(null);
+  const [renderer, setRenderer] = useState(null);
+
   useEffect(() => {
-    const renderer = new THREE.WebGLRenderer();
-    renderer.setSize(window.innerWidth, window.innerHeight);
-    const scene = new THREE.Scene();
-    const camera = new THREE.PerspectiveCamera(
-      75, window.innerWidth / window.innerHeight, 0.1, 1000,
-    );
-    renderer.render(scene, camera);
-    mount.current.appendChild(renderer.domElement);
-  }, []);
+    if (!renderer) {
+      const newRenderer = new THREE.WebGLRenderer();
+      newRenderer.setSize(window.innerWidth, window.innerHeight);
+      setRenderer(newRenderer);
+    }
+  }, [renderer]);
+
+  useEffect(() => {
+    if (renderer) {
+      const scene = new THREE.Scene();
+      const axesHelper = new THREE.AxesHelper(5);
+      scene.add(axesHelper);
+      const camera = new THREE.PerspectiveCamera(
+        75, window.innerWidth / window.innerHeight, 0.2, 1000,
+      );
+      const boxGeometry = new THREE.BoxGeometry(0.1, 0.1, 0.1);
+      const boxMaterial = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+      const cube = new THREE.Mesh(boxGeometry, boxMaterial);
+      scene.add(cube);
+      camera.position.set(0,0,0.3);
+      const animate = () => {
+        requestAnimationFrame(animate);
+        cube.rotation.x += 0.05;
+        renderer.render(scene, camera);
+      }
+      animate();
+      renderer.render(scene, camera);
+      mount.current.appendChild(renderer.domElement);
+    }
+  }, [renderer]);
 
   return <div ref={mount} />;
 };
