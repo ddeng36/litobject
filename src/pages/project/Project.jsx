996b73c2421ec614321ae0664bebdbce90c65314
Dingchao <dingchaodeng@gatech.edu>
2023-06-13 18:07:10 +0800

    light

--- src/pages/project/Project.jsx
+++ src/pages/project/Project.jsx
@@ -1,6 +1,7 @@
 import React, { useRef, useEffect, useState } from "react";
 import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+import * as dat from "dat.gui";
 const Project = () => {
   const mount = useRef(null);
   const [renderer, setRenderer] = useState(null);
@@ -16,7 +17,7 @@ const Project = () => {
   useEffect(() => {
     if (renderer) {
       const scene = new THREE.Scene();
-      const axesHelper = new THREE.AxesHelper(5);
+      const axesHelper = new THREE.AxesHelper(100);
       scene.add(axesHelper);
       const camera = new THREE.PerspectiveCamera(
         75,
@@ -24,15 +25,41 @@ const Project = () => {
         0.2,
         1000
       );
-      
+
       //cube
       const boxGeometry = new THREE.BoxGeometry(5, 5, 5);
       const boxMaterial = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
       const cube = new THREE.Mesh(boxGeometry, boxMaterial);
       scene.add(cube);
 
+      //sphere
+      const sphereGeometry = new THREE.SphereGeometry(5, 30, 30);
+      const sphereMaterial = new THREE.MeshStandardMaterial({
+        color: 0xffff00,
+        wireframe: false,
+      });
+      const sphere = new THREE.Mesh(sphereGeometry, sphereMaterial);
+      scene.add(sphere);
+
+      //gui
+      const gui = new dat.GUI();
+      const options ={
+        sphereColor: 0xffff00,
+        wireframe: false,
+        speed: 0.001
+      }
+      gui.addColor(options, 'sphereColor').onChange(()=>{
+        sphereMaterial.color.set(options.sphereColor)
+      })
+      gui.add(options, 'wireframe').onChange(()=>{
+        sphereMaterial.wireframe = options.wireframe
+      })
+      gui.add(options, 'speed', 0, 0.5).onChange(()=>{
+        options.speed = options.speed
+      })
+
       //plane
-      const planeGeometry = new THREE.PlaneGeometry(30,30);
+      const planeGeometry = new THREE.PlaneGeometry(30, 30);
       const planeMaterial = new THREE.MeshBasicMaterial({
         color: 0xffffff,
         side: THREE.DoubleSide,
@@ -42,14 +69,27 @@ const Project = () => {
       scene.add(plane);
 
       //gridHelprt
-      const gridHelper = new THREE.GridHelper(30,30);
+      const gridHelper = new THREE.GridHelper(30, 30);
       scene.add(gridHelper);
 
 
-      camera.position.set(-10, 30, 30);
+      //ambientlight
+      const ambientLight = new THREE.AmbientLight(0x333333, );
+      scene.add(ambientLight);
+
+      //directionalLight
+      const directionalLight = new THREE.DirectionalLight(0xffffff);
+      scene.add(directionalLight);
+
+      //camera
+      camera.position.set(10, 10, 10);
+
+
+      
       const animate = () => {
         requestAnimationFrame(animate);
         cube.rotation.x += 0.01;
+        sphere.position.y += options.speed;
         renderer.render(scene, camera);
       };
       animate();
